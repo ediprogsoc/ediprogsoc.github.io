41421bd64fc73c17de0c693524dfaca0d39ecdc3
Elijah Gutierrez <s1740779@ed.ac.uk>
2020-05-03 14:15:15 +0200

    Fixed issue w/ navbar on safari

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,26 @@ var prev_pos;
 
 window.addEventListener("scroll", function() {
 
-
   curr_pos = document.body.getBoundingClientRect().top;
+
+  console.log(curr_pos, prev_pos);
+
   if (curr_pos < prev_pos) {
     $('.navbar').fadeOut();
   }
+
   else if (curr_pos > prev_pos){
-    $('.navbar').fadeIn();
+    $('.navbar').show();
   }
+
+  if (curr_pos >= 0) {
+    curr_pos = 0;
+  }
+
+  if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+        $('.navbar').hide();
+    }
+
   prev_pos = curr_pos;
+
   });
